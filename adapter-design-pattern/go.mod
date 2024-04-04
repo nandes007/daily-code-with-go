096b852af11fec34ece339eac8b6e3281aa1b398
Nandes Simanjuntak <nandessimanjuntak1803@gmail.com>
2024-04-04 08:25:29 +0700

    adapter design pattern

---
+++ adapter-design-pattern/go.mod
@@ -0,0 +1,3 @@
+module golangbyexample.com/adapter-design-pattern
+
+go 1.22.0
---
+++ adapter-design-pattern/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	client := &client{}
+	mac := &mac{}
+	client.insertSquareUsbInComputer(mac)
+	windowsMachine := &windows{}
+	windowsMachineAdapter := &windowsAdapter{
+		windowMachine: windowsMachine,
+	}
+	client.insertSquareUsbInComputer(windowsMachineAdapter)
+}
