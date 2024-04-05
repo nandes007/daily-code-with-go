6c009165b7e7d29aa599ff7904613d4a91162cde
Nandes Simanjuntak <nandessimanjuntak1803@gmail.com>
2024-04-05 09:04:08 +0700

    proxy design pattern

---
+++ proxy-design-pattern/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	nginxServer := newNginxServer()
+	appStatusURL := "/app/status"
+	craeteuserURL := "/create/user"
+	httpCode, body := nginxServer.handleRequest(appStatusURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+	httpCode, body = nginxServer.handleRequest(appStatusURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+	httpCode, body = nginxServer.handleRequest(appStatusURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+	httpCode, body = nginxServer.handleRequest(craeteuserURL, "POST")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+	httpCode, body = nginxServer.handleRequest(craeteuserURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+
+}
